10d91057e6b71abdcbb70c8af72a861f36ed9486
Asher Hamrick <ashhamrick2@gmail.com>
2020-03-25 00:11:27 -0400

    Basic Login System UI

---
+++ MemoryBank/Landing.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MemoryBank
+{
+    public partial class Landing : ContentPage
+    {
+        public Landing()
+        {
+            InitializeComponent();
+        }
+
+        async void Register(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushModalAsync(new Register());
+        }
+
+        void Login(System.Object sender, System.EventArgs e)
+        {
+        }
+        
+        void Reset_Pass(System.Object sender, System.EventArgs e)
+        {
+        }
+    }
+}
---
+++ MemoryBank/Register.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MemoryBank
+{
+    public partial class Register : ContentPage
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        async void Back(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        void RegisterFunc(System.Object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
