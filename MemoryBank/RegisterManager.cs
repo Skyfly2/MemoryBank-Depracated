3939919d92af0cc6aa42db18bd5bc2b66fb8049f
Asher Hamrick <ashhamrick2@gmail.com>
2020-03-25 22:18:27 -0400

    Connection to Cosmos DB

---
+++ MemoryBank/RegisterManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+
+namespace MemoryBank
+{
+    public partial class RegisterManager
+    {
+        static RegisterManager def = new RegisterManager();
+
+        const string accountURL = @"https://memorybank.documents.azure.com:443/";
+        const string primaryKey = @"5HCoinu3i0IHXtp6g1U4XCN7mdd3SdWlNOfLiCgtwrYElYq2C4kCXYeLmUh0nSCQwH6PvcSasaiyrMLH4JYVRQ==";
+        const string databaseId = @"MemoryBank";
+        const string collection = @"Registration";
+
+        private Uri connect = UriFactory.CreateDocumentCollectionUri(databaseId, collection);
+
+        private DocumentClient client;
+
+        private RegisterManager()
+        {
+            client = new DocumentClient(new System.Uri(accountURL), primaryKey);
+        }
+
+        public static RegisterManager Default{
+            get { return def; }
+            set { def = value; }
+            }
+
+
+    }
+}
---
+++ MemoryBank/Registration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace MemoryBank
+{
+	public class Registration
+	{
+		[JsonProperty(PropertyName = "Email")]
+		public string Email { get; set; }
+
+		[JsonProperty(PropertyName = "Pass")]
+		public string Pass { get; set; }
+
+        [JsonProperty(PropertyName = "ConfirmPass")]
+        public string CPass { get; set; }
+
+		[JsonProperty(PropertyName = "First")]
+		public string First { get; set; }
+
+        [JsonProperty(PropertyName = "Last")]
+        public string Last { get; set; }
+	}
+}
