dec2e8d2eb68893c07c4b2c563a5bb0477af0615
Asher Hamrick <ashhamrick2@gmail.com>
2020-03-25 22:19:03 -0400

    Pre-Data Running Registration Infrastructure

--- MemoryBank/Register.xaml.cs
+++ MemoryBank/Register.xaml.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace MemoryBank
 {
     public partial class Register : ContentPage
     {
+        RegisterManager manage;
         public Register()
         {
             InitializeComponent();
+            manage = RegisterManager.Default;
         }
 
         async void Back(System.Object sender, System.EventArgs e)
@@ -19,7 +21,35 @@ namespace MemoryBank
 
         void RegisterFunc(System.Object sender, System.EventArgs e)
         {
+            var info = new Registration { First = firstname.Text };
+            info.Last = lastname.Text;
+            info.Pass = pass.Text;
+            info.CPass = cpass.Text;
+            info.Email = email.Text;
 
+            if ((info.Pass == info.CPass)){
+                if (!String.IsNullOrWhiteSpace(info.Last) && !String.IsNullOrWhiteSpace(info.First) && !String.IsNullOrWhiteSpace(info.Email) && !String.IsNullOrWhiteSpace(info.Pass))
+                {
+                    Reg(info);
+                }
+                else
+                {
+                    DisplayAlert("Registration Error", "Please Fill Out All Fields", "Close");
+                }
+            }
+            else
+            {
+                cpass.Text = "";
+                pass.Text = "";
+                DisplayAlert("Registration Error", "Passwords Do Not Match", "Close");
+                
+            }
+
+        }
+
+        void Reg(Registration info)
+        {
+      
         }
     }
 }
