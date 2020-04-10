97e6a9e1f042cf3ce59af472c02cbb156ac42d19
Asher Hamrick <ashhamrick2@gmail.com>
2020-04-09 22:47:02 -0400

    Basic NotesPage UI
    Content will be added dynamically with C# later

---
+++ MemoryBank/NotesPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MemoryBank
+{
+    public partial class NotesPage : ContentPage
+    {
+        public NotesPage()
+        {
+            InitializeComponent();
+        }
+
+        void NewDocument(System.Object sender, System.EventArgs e)
+        {
+        }
+    }
+}
