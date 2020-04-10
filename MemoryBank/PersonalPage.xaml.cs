1f3e18294f534b9f98343c7b079fa6fac04a1648
Asher Hamrick <ashhamrick2@gmail.com>
2020-04-09 22:43:26 -0400

    PersonalPage Basic UI
    Dynamic content will be written and added with C# later

---
+++ MemoryBank/PersonalPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MemoryBank
+{
+    public partial class PersonalPage : ContentPage
+    {
+        public PersonalPage()
+        {
+            InitializeComponent();
+            name.Text = "Name: " + Account.First() + " " + Account.Last();
+            email.Text = Account.Email();
+        }
+
+        void NewPersonalInfo(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        void UpdateData(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        void EditSSN(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        void ViewSSN(System.Object sender, System.EventArgs e)
+        {
+        }
+    }
+}
