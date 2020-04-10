bc95130e358f4fd43c2a83396301a3b7110798d6
Asher Hamrick <ashhamrick2@gmail.com>
2020-04-09 23:04:02 -0400

    Basic Search Page UI

---
+++ MemoryBank/SearchPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MemoryBank
+{
+    public partial class SearchPage : ContentPage
+    {
+        public SearchPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Search(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        async void ViewContent(System.Object sender, System.EventArgs e)
+        {
+        }
+    }
+}
