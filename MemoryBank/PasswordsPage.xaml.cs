365bcd530facdc9368a6ff50679a206eebd7bbb6
Asher Hamrick <ashhamrick2@gmail.com>
2020-04-09 22:19:18 -0400

    Basic PasswordsPage UI
    Content will be transferred to dynamic writing in C# soon

---
+++ MemoryBank/PasswordsPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MemoryBank
+{
+    public partial class PasswordsPage : ContentPage
+    {
+        public PasswordsPage()
+        {
+            InitializeComponent();
+        }
+
+        void NewPass(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        void EditPass(System.Object sender, System.EventArgs e)
+        {
+        }
+    }
+}
